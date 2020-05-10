bb42ad4d8cb82565499b336e572320462e40894b
AlanWilian <awmoliveira@gmail.com>
2020-05-10 21:03:47 +0100

    example with predicate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 
 namespace Delegates
 {
@@ -31,6 +32,38 @@ namespace Delegates
             //BinaryNumericOperation op = CalculationService.ShowSum;
             //op += CalculationService.ShowMax;
             //op(a, b);
+
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            list.ForEach(p => { p.Price += p.Price * 0.1; });
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+            list.ForEach(UpdatePrice);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+
+        }
+
+        private static void UpdatePrice(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
 
         static void RemoveRedEyeFilter(Photo photo)
